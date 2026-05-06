a8f93e505a4cb269f3abd0ad8d470969d7901631
Anthony Perez <aperez327@gmail.com>
2026-05-05 21:33:37 -0400

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by Anthony Perez on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
